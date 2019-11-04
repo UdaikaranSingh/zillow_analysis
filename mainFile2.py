69e8838180a5169ec552a20c889311f6e28f1d81
Udaikaran Singh <udaisingh@Udaikarans-MBP.lan1>
2019-11-03 18:25:44 -0800

    added distribution code

--- mainFile2.py
+++ mainFile2.py
@@ -3,18 +3,20 @@ import os
 from subprocess import Popen
 import zipfile
 
-from task2_3a import *
+from task4 import *
 gen_directory = "/Users/udaisingh/Desktop/gen"
 
-#FIPS = ["02"]
+FIPS = ["02"]
 
+"""
 FIPS = ["01","02", "04", "05", "06", "08",
 "09", "10", "11", "12", "13", "15", "16", "17",
 "18", "19", "20", "21", "22", "23", "24", "25",
 "26", "27", "28", "29", "30", "31", "32", "33",
 "34", "35", "36", "37", "38", "39", "40", "41",
 "42", "44", "45", "46", "47", "48", "49", "50",
-"51", "53", "54", "55", "56", "66"]
+"51", "53", "54", "55", "56"]
+"""
 
 def process(fip):
     print("Current FIP processed: " + fip)
@@ -38,7 +40,8 @@ def process(fip):
     os.system(remove_zip_file)
 
     print("Program Begining")
-    program(fip, gen_directory)
+    gen_dir = os.path.join(gen_directory, fip)
+    program(fip, gen_dir)
     print("Program Ended")
 
     remove_folder_command = "rm -rf " + fip + "/"
