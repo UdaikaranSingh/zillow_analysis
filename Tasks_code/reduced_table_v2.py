4d5a8818c2e60760658caec28ed2dfe9f0e44fe2
Udaikaran Singh <udaisingh@Udaikarans-MacBook-Pro.local>
2019-11-12 21:09:13 -0800

    reduced table file fized

--- Tasks_code/reduced_table_v2.py
+++ Tasks_code/reduced_table_v2.py
@@ -14,6 +14,30 @@ def program(fips_code, gen_dir = os.getcwd()):
               'IntraFamilyTransferFlag','LoanTypeStndCode','PropertyUseStndCode',
              'RecordTypeStndCode','SalesPriceAmount','SalesPriceAmountStndCode'])
 
+	date_table = SingleTableReader("ZTrans", "Main", file_dir).read(
+		usecols =["TransId", "DocumentDate", "SignatureDate", "RecordingDate"])
+
+	def get_primary_date(row):
+	    if not pd.isna(row['DocumentDate']):
+	        return 0
+	    elif not pd.isna(row['SignatureDate']):
+	        return 1
+	    else:
+	        return 2
+
+	def get_primary_date_marker(row):
+	    if not pd.isna(row['DocumentDate']):
+	        return row['DocumentDate']
+	    elif not pd.isna(row['SignatureDate']):
+	        return row['SignatureDate']
+	    else:
+	        return np.nan
+
+    primary_dates_marker = date_table.apply(get_primary_date_marker, axis = 1)
+    priamry_dates = date_table.apply(get_primary_date, axis = 1)
+	main_table['primary_date'] = priamry_dates
+	main_table['primary_date_marker'] = primary_dates_marker
+
 	prop_table = SingleTableReader("ZTrans", "PropertyInfo", file_dir).read(
 		usecols = ['TransId','AssessorParcelNumber',
 		'ImportParcelID',
